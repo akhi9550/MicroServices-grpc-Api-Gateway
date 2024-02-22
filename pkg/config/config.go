d154b067428d65897e4d76eb6daa5ca97b5a2625
akhi9550 <akhilc248@gmail.com>
2024-02-22 12:42:00 +0530

    grpc-api-gateway

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -7,7 +7,6 @@ type Config struct {
 	AuthSvcUrl    string `mapstructure:"AUTH_SVC_URL"`
 	ProductSvcUrl string `mapstructure:"PRODUCT_SVC_URL"`
 	OrderSvcUrl   string `mapstructure:"ORDER_SVC_URL"`
-	CartSvcUrl    string `mapstructure:"CART_SVC_URL"`
 }
 
 func LoadConfig() (c Config, err error) {
